059a86908029e51230c36d56e3bbef643d9f8279
LCabello <ljacalfer@gmail.com>
2016-12-08 12:53:12 +0100

    Modifications required to read and write more than ten million lines

--- escapeString/commentTraductor.js
+++ escapeString/commentTraductor.js
@@ -6,33 +6,35 @@ const readline = require('readline');
 /* You should introduce your file path*/
 let fileName = 'test.txt';
 
-var logger = fs.createWriteStream('log.txt', {
+var logger = fs.createWriteStream('output.txt', {
   flags: 'w'
 });
 
 var data = [];
+
+var escapeNote = function(text){
+  return text.split(' ').map(function(word){ return encodeURI(word); }).join(' ');
+}
+
+var unescapeNote = function(text){
+  return text.split(' ').map(function(word){ return decodeURI(word); }).join(' ');
+}
+
+console.log('Start : ', new Date());
 
 var reading = function(){
   let lineReader = readline.createInterface({
     input: fs.createReadStream(fileName)
   });
 
-  console.log('Reading...');
-
   lineReader.on('line', function(line){
     //It will return all lines in the node consle
-    let newComment = encodeURI(unescape(line));
-    data.push(newComment);
+    let newComment = unescape(line);
+    logger.write(escapeNote(newComment) + '\n');
   });
 
-  console.log('Writing...');
-
   lineReader.on('close', function(){
-    data.forEach(function(comment){
-      logger.write(comment + '\n');
-    });
-
-    console.log('Finish!');
+    console.log('Finish: ' + new Date());
   });
 }();
 
