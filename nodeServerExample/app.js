cdb0f69dc7151b76c188bda64991e6b6dda22629
LCabello <luis.cab.fer@gmail.com>
2020-02-24 15:34:20 +0100

    Sending responses

--- nodeServerExample/app.js
+++ nodeServerExample/app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 const port = 3000;
 
