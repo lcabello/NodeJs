bb8565d824a4d8d72d4ca4e34a9f18f52a902f1e
LCabello <luis.cab.fer@gmail.com>
2020-02-25 11:51:09 +0100

    Redirect request after enter message from client

--- nodeServerExample/app.js
+++ nodeServerExample/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
 
   if (url === '/') {
     res.write('<html>');
@@ -11,6 +14,14 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'Dummy');
+    // res.writeHead(302, {});
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
